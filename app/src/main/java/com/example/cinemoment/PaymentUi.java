ab7fc84c100a84d6e5068b36f80b288af33e06eb
MaheshBawantha <maheshbawantha1998@gmail.com>
2021-05-09 14:16:47 +0530

    PaymentUi done. recycler view ongoing.

--- app/src/main/java/com/example/cinemoment/PaymentUi.java
+++ app/src/main/java/com/example/cinemoment/PaymentUi.java
@@ -1,24 +1,46 @@
 package com.example.cinemoment;
 
 import android.annotation.SuppressLint;
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.ProgressBar;
 import android.widget.TextView;
+import android.widget.Toast;
+
 
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.OnProgressListener;
+import com.google.firebase.storage.StorageReference;
+import com.google.firebase.storage.UploadTask;
+
+import java.util.HashMap;
 
 public class PaymentUi extends AppCompatActivity {
-
-    TextView pName,pPrice,pAdvance;
-    private DatabaseReference dbRef;
-
+    private static final int REQUEST_CODE_IMAGE =101 ;
+    private ImageView imageViewAdd;
+    private EditText inputName3;
+    private TextView textViewProgress;
+    private ProgressBar progressBar;
+    private Button btnUpload;
+    Uri imageUri;
+    boolean isImageAdded = false;
+    DatabaseReference dbRef;
+    StorageReference storageRef;
 
 
     @Override
@@ -26,32 +48,94 @@ public class PaymentUi extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_payment_ui);
 
-        pName = findViewById(R.id.pName);
-        pPrice = findViewById(R.id.pPrice);
-        pAdvance = findViewById(R.id.pAdvance);
-        Button viewPD = findViewById(R.id.viewPD);
+        imageViewAdd = findViewById(R.id.imageViewAdd);
+        inputName3 = findViewById(R.id.inputName3);
+        textViewProgress = findViewById(R.id.textViewProgress);
+        progressBar = findViewById(R.id.progressBar);
+        btnUpload = findViewById(R.id.btnUpload);
 
-        dbRef = FirebaseDatabase.getInstance("https://cinemoment-8c5c9-default-rtdb.firebaseio.com/").getReference().child("Package");
+        textViewProgress.setVisibility(View.GONE);
+        progressBar.setVisibility(View.GONE);
 
-        viewPD.setOnClickListener(v -> dbRef.addValueEventListener(new ValueEventListener() {
-            @SuppressLint("SetTextI18n")
+        dbRef = FirebaseDatabase.getInstance("https://cinemoment-8c5c9-default-rtdb.firebaseio.com/").getReference().child("Payment");
+        storageRef = FirebaseStorage.getInstance("gs://cinemoment-8c5c9.appspot.com/").getReference().child("PaySlip");
+
+
+
+        imageViewAdd.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
-                Package aPackage=dataSnapshot.getValue(Package.class);
-
-                assert aPackage != null;
-                pName.setText("Package:  "+aPackage.getPName());
-                pPrice.setText("Price:  "+aPackage.getPPrice());
-                pAdvance.setText("Advance:  "+aPackage.getPAdvance());
-
-
+            public void onClick(View v) {
+                Intent intent = new Intent();
+                intent.setType("image/*");
+                intent.setAction(Intent.ACTION_GET_CONTENT);
+                startActivityForResult(intent,REQUEST_CODE_IMAGE);
+            }
+        });
+        btnUpload.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                final String payer = inputName3.getText().toString();
+                if (isImageAdded != false && payer != null)
+                {
+                    uploadImage(payer);
+                }
 
             }
+        });
 
+
+    }
+
+    private void uploadImage(final String payer) {
+        textViewProgress.setVisibility(View.VISIBLE);
+        progressBar.setVisibility(View.VISIBLE);
+        final String key = dbRef.push().getKey();
+        storageRef.child(key+".jpg").putFile(imageUri).addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
             @Override
-            public void onCancelled(@NonNull DatabaseError error) {
+            public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
+                storageRef.child(key+".jpg").getDownloadUrl().addOnSuccessListener(new OnSuccessListener<Uri>() {
+                    @Override
+                    public void onSuccess(Uri uri) {
+                        HashMap hashMap = new HashMap();
+                        hashMap.put("PayerName",payer);
+                        hashMap.put("ImageUrl",uri.toString());
+
+                        dbRef.child(key).setValue(hashMap).addOnSuccessListener(new OnSuccessListener<Void>() {
+                            @Override
+                            public void onSuccess(Void aVoid) {
+                                Toast.makeText(PaymentUi.this, "Payment Method Successfully", Toast.LENGTH_SHORT).show();
+                            }
+                        });
+
+                    }
+                });
 
             }
-        }));
+        }).addOnProgressListener(new OnProgressListener<UploadTask.TaskSnapshot>() {
+            @Override
+            public void onProgress(@NonNull UploadTask.TaskSnapshot taskSnapshot) {
+                double progress = (taskSnapshot.getBytesTransferred()*100)/taskSnapshot.getTotalByteCount();
+                progressBar.setProgress((int) progress);
+                textViewProgress.setText(progress+" %");
+
+            }
+        });
+
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == REQUEST_CODE_IMAGE && data != null){
+            imageUri = data.getData();
+            isImageAdded = true;
+            imageViewAdd.setImageURI(imageUri);
+        }
+
+    }
+    public void successful(View view)
+    {
+        Intent i =new Intent(this,BookingSuccess.class);
+        startActivity(i);
     }
 }
